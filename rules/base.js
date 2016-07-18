02587ca8539e801742d34f90057d0fdcd10ed01f
Plusb Preco <plusb21@gmail.com>
2016-07-18 18:34:47 +0900

    Disable `no-invalid-this` rule
    Until `Class Properties` issue has resolved.

--- rules/base.js
+++ rules/base.js
@@ -72,7 +72,8 @@ module.exports = {
     'no-implicit-coercion': [2, {boolean: false}],
     'no-implicit-globals': 2,
     'no-implied-eval': 2,
-    'no-invalid-this': 2,
+    // issue: https://github.com/babel/eslint-plugin-babel/issues/12
+    // 'no-invalid-this': 2,
     'no-iterator': 2,
     'no-labels': 2,
     'no-lone-blocks': 2,
