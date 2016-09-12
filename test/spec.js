788f4f97cce7cb36ff90a308f3cdf444347433df
Plusb Preco <plusb21@gmail.com>
2016-09-12 11:23:57 +0900

    Clearify test descriptions

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import isPlainObj from 'is-plain-obj';
 
-test('base rule is valid config', (t) => {
+test('`rules/base.js` is valid config', (t) => {
   const config = require('../rules/base');
 
   t.true(isPlainObj(config));
@@ -10,7 +10,7 @@ test('base rule is valid config', (t) => {
   t.true(isPlainObj(config.rules));
 });
 
-test('esnext rule is valid config', (t) => {
+test('`rules/esnext.js` is valid config', (t) => {
   const config = require('../rules/esnext');
 
   t.true(isPlainObj(config));
@@ -19,7 +19,7 @@ test('esnext rule is valid config', (t) => {
   t.true(isPlainObj(config.rules));
 });
 
-test('import rule is valid config', (t) => {
+test('`rules/import.js` is valid config', (t) => {
   const config = require('../rules/import');
 
   t.true(isPlainObj(config));
@@ -29,7 +29,7 @@ test('import rule is valid config', (t) => {
   t.true(isPlainObj(config.rules));
 });
 
-test('react rule is valid config', (t) => {
+test('`rules/react.js` is valid config', (t) => {
   const config = require('../rules/react');
 
   t.true(isPlainObj(config));
@@ -38,14 +38,14 @@ test('react rule is valid config', (t) => {
   t.true(isPlainObj(config.rules));
 });
 
-test('primary index config is valid config', (t) => {
+test('`index.js` is valid config', (t) => {
   const config = require('../');
 
   t.true(isPlainObj(config));
   t.true(Array.isArray(config.extends));
 });
 
-test('primary react config is valid config', (t) => {
+test('`react.js` is valid config', (t) => {
   const config = require('../react');
 
   t.true(isPlainObj(config));
