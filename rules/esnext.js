689b7aa362b470b4e6445341e99b2c26c7a0b79c
Plusb Preco <plusb21@gmail.com>
2016-09-24 23:26:17 +0900

    Add `experimentalObjectRestSpread ` option

--- rules/esnext.js
+++ rules/esnext.js
@@ -8,6 +8,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
   },
   rules: {
     // disable original eslint rules
