3c6b9351f5bf5efe2bb52b53a6b9a275312d5592
Plusb Preco <plusb21@gmail.com>
2016-11-15 10:26:10 +0900

    Update `react/no-unused-prop-types` rule

--- rules/react.js
+++ rules/react.js
@@ -34,7 +34,7 @@ module.exports = {
     'react/no-string-refs': 'error',
     'react/no-unescaped-entities': 'error',
     'react/no-unknown-property': 'error',
-    'react/no-unused-prop-types': ['error', {skipShapeProps: true}],
+    'react/no-unused-prop-types': 'error',
     'react/prefer-es6-class': 'error',
     'react/prefer-stateless-function': 'error',
     'react/prop-types': 'error',
