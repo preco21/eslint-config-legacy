40095cce609a6654479d4076421d8f916c07e0d9
Plusb Preco <plusb21@gmail.com>
2018-03-05 23:20:42 +0900

    Add `react/jsx-max-depth` rule

--- rules/react.js
+++ rules/react.js
@@ -74,6 +74,7 @@ module.exports = {
     'react/jsx-indent': ['error', 2],
     'react/jsx-indent-props': ['error', 2],
     'react/jsx-key': 'error',
+    'react/jsx-max-depth': 'off',
     'react/jsx-max-props-per-line': ['error', {when: 'multiline'}],
     'react/jsx-no-bind': ['error', {allowArrowFunctions: true}],
     'react/jsx-no-comment-textnodes': 'error',
