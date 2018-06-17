9e919b8f8efadd622ef0b9c2bd5fe0c298575887
Plusb Preco <plusb21@gmail.com>
2018-06-17 23:31:51 +0900

    Reorder rule `import/no-useless-path-segments`

--- rules/import.js
+++ rules/import.js
@@ -30,6 +30,7 @@ module.exports = {
     'import/no-webpack-loader-syntax': 'error',
     'import/no-self-import': 'error',
     'import/no-cycle': 'error',
+    'import/no-useless-path-segments': 'error',
 
     // Helpful warnings
     'import/export': 'error',
@@ -64,7 +65,6 @@ module.exports = {
     'import/prefer-default-export': 'error',
     'import/max-dependencies': 'off',
     'import/no-unassigned-import': 'off',
-    'import/no-useless-path-segments': 'error',
     'import/no-named-default': 'error',
     'import/no-default-export': 'off',
     'import/no-anonymous-default-export': [
