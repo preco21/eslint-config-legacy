e635281ab510aa0d7d55f99d92a73ba26b4caa79
Plusb Preco <plusb21@gmail.com>
2019-06-26 00:23:16 +0900

    Add new node rules

---
+++ rules/node.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// prettier-ignore
+module.exports = {
+  plugins: ['node'],
+  rules: {
+    // possible errors
+    'node/no-extraneous-import': 'off',
+    'node/no-extraneous-require': 'off',
+    'node/no-missing-import': 'off',
+    'node/no-missing-require': 'off',
+    'node/no-unpublished-bin': 'error',
+    'node/no-unpublished-import': 'off',
+    'node/no-unpublished-require': 'off',
+    'node/no-unsupported-features/es-builtins': 'error',
+    'node/no-unsupported-features/es-syntax': 'error',
+    'node/no-unsupported-features/node-builtins': 'error',
+    'node/process-exit-as-throw': 'error',
+    'node/shebang': 'error',
+
+    // best practices
+    'node/no-deprecated-api': 'error',
+
+    // stylistic issues
+    'node/exports-style': 'off',
+    'node/file-extension-in-import': 'off',
+    'node/prefer-global/buffer': 'off',
+    'node/prefer-global/console': 'off',
+    'node/prefer-global/process': 'off',
+    'node/prefer-global/url-search-params': 'off',
+    'node/prefer-global/url': 'off',
+    'node/prefer-promises/dns': 'off',
+    'node/prefer-promises/fs': 'off',
+  },
+}
