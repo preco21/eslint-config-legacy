1e3993ff5136cc5efd7841ba38aef1aeff4aaca0
Plusb Preco <plusb21@gmail.com>
2021-06-24 02:11:14 +0900

    feat: add brand new `possible-errors` rules

---
+++ rules/base/possible-errors.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/**
+ * possible errors
+ * > https://eslint.org/docs/rules/#possible-errors
+ */
+module.exports = {
+  rules: {
+    'for-direction': 'error',
+    'getter-return': ['error', { allowImplicit: true }],
+    'no-async-promise-executor': 'error',
+    'no-await-in-loop': 'warn',
+    'no-compare-neg-zero': 'error',
+    'no-cond-assign': 'error',
+    'no-console': 'warn',
+    'no-constant-condition': 'warn',
+    'no-control-regex': 'error',
+    'no-debugger': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-else-if': 'error',
+    'no-dupe-keys': 'error',
+    'no-duplicate-case': 'error',
+    'no-empty': 'error',
+    'no-empty-character-class': 'error',
+    'no-ex-assign': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-extra-parens': ['error', 'all', {
+      conditionalAssign: false,
+      returnAssign: false,
+      nestedBinaryExpressions: false,
+      // delegate to `eslint-plugin-react`
+      ignoreJSX: 'all',
+      enforceForArrowConditionals: false,
+      enforceForSequenceExpressions: false,
+      enforceForNewInMemberExpressions: false,
+      enforceForFunctionPrototypeMethods: false,
+    }],
+    'no-extra-semi': 'error',
+    'no-func-assign': 'error',
+    'no-import-assign': 'error',
+    'no-inner-declarations': 'error',
+    'no-invalid-regexp': 'error',
+    'no-irregular-whitespace': 'error',
+    'no-loss-of-precision': 'warn',
+    'no-misleading-character-class': 'error',
+    'no-obj-calls': 'error',
+    'no-promise-executor-return': 'error',
+    'no-prototype-builtins': 'error',
+    'no-regex-spaces': 'error',
+    'no-setter-return': 'error',
+    'no-sparse-arrays': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'no-unreachable-loop': 'error',
+    'no-unsafe-finally': 'error',
+    'no-unsafe-negation': 'error',
+    'no-unsafe-optional-chaining': 'error',
+    'no-useless-backreference': 'error',
+    // TODO: enable once the issue regarding this rule resolved
+    // https://github.com/eslint/eslint/issues/11899
+    'require-atomic-updates': 'off',
+    'use-isnan': 'error',
+    'valid-typeof': ['error', { requireStringLiterals: true }],
+  },
+};
