5c813be5f7c5508bca621f82f6becb707fee5424
Plusb Preco <plusb21@gmail.com>
2021-06-29 00:44:36 +0900

    feat: add brand new `strict-mode` rules

---
+++ rules/base/strict-mode.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * strict mode
+ * > https://eslint.org/docs/rules/#strict-mode
+ */
+module.exports = {
+  rules: {
+    // this rule is intentionally turned off: since use of modern ECMAScript
+    // modules or tools like Babel automatically assumes environments to be
+    // `strict-mode` enabled, we don't need to force users to have a strict mode
+    // flag in every entry of their code.
+    'strict': 'off',
+  },
+};
