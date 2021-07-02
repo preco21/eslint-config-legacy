4299fb020465e3262dc14bee567575a49993ab9a
Plusb Preco <plusb21@gmail.com>
2021-07-02 12:49:50 +0900

    fix: should allow constant condition in loops

--- rules/base/possible-errors.js
+++ rules/base/possible-errors.js
@@ -13,7 +13,7 @@ module.exports = {
     'no-compare-neg-zero': 'error',
     'no-cond-assign': 'error',
     'no-console': 'warn',
-    'no-constant-condition': 'warn',
+    'no-constant-condition': ['warn', { checkLoops: false }],
     'no-control-regex': 'error',
     'no-debugger': 'error',
     'no-dupe-args': 'error',
